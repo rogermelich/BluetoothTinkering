ce64e5622b57cd6e224856101fe4baeadef9d4f3
rogermelich <rogermelich@gmail.com>
2016-02-16 19:22:14 +0100

    finishing OnActiveteResult code

--- app/src/main/java/roger/dam2/iesebre/com/bluetoothtinkering/MainActivity.java
+++ app/src/main/java/roger/dam2/iesebre/com/bluetoothtinkering/MainActivity.java
@@ -1,13 +1,17 @@
 package roger.dam2.iesebre.com.bluetoothtinkering;
 
+import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int REQUEST_ENABLE_BT = 1;
+    private static final String TAG = "MainActivity";
     BluetoothAdapter mBluetoothAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
         BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if (mBluetoothAdapter == null) {
         // Device does not support Bluetooth
-            Toast.makeText(this, "Bluetooth is not available", Toast.LENGTH_LONG.show());
+            Toast.makeText(this, "Bluetooth is not available", Toast.LENGTH_LONG).show();
         } else {
             Toast.makeText(this, "Bluetooth is available", Toast.LENGTH_LONG).show();
         }
@@ -33,4 +37,21 @@ public class MainActivity extends AppCompatActivity {
             startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
 
         }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode){
+            case REQUEST_ENABLE_BT:
+                if (resultCode == Activity.RESULT_OK) {
+                    Toast.makeText(this, "Bluetooth is active", Toast.LENGTH_LONG).show();
+                } else {
+                    Log.d(TAG, "BT not enabled");
+                    Toast.makeText(this, "El Bluetooth no s'ha activat correctament", Toast.LENGTH_LONG).show();
+                    this.finish();
+                }
+        }
+    }
 }
