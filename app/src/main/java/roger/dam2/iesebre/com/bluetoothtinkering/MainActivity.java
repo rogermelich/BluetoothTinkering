9de852d7bb7ce6971bdbe7bdf7cd4ccc0bf2b6a5
rogermelich <rogermelich@gmail.com>
2016-02-16 19:14:53 +0100

    enble bluetooth test

--- app/src/main/java/roger/dam2/iesebre/com/bluetoothtinkering/MainActivity.java
+++ app/src/main/java/roger/dam2/iesebre/com/bluetoothtinkering/MainActivity.java
@@ -1,11 +1,14 @@
 package roger.dam2.iesebre.com.bluetoothtinkering;
 
 import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    BluetoothAdapter mBluetoothAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,7 +17,20 @@ public class MainActivity extends AppCompatActivity {
         BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if (mBluetoothAdapter == null) {
         // Device does not support Bluetooth
+            Toast.makeText(this, "Bluetooth is not available", Toast.LENGTH_LONG.show());
+        } else {
+            Toast.makeText(this, "Bluetooth is available", Toast.LENGTH_LONG).show();
         }
 
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+
+        }
 }
