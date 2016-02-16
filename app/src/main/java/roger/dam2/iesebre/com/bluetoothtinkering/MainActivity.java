4ce1f49db7de80093d1bed6dff8e516ca55701a2
rogermelich <rogermelich@gmail.com>
2016-02-16 19:38:20 +0100

    Crear on createoptionsmenu and onoptionsitemselected

--- app/src/main/java/roger/dam2/iesebre/com/bluetoothtinkering/MainActivity.java
+++ app/src/main/java/roger/dam2/iesebre/com/bluetoothtinkering/MainActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -26,6 +28,10 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "Bluetooth is available", Toast.LENGTH_LONG).show();
         }
 
+        Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+        discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
+        startActivity(discoverableIntent);
+
     }
 
     @Override
@@ -35,7 +41,6 @@ public class MainActivity extends AppCompatActivity {
         if (!mBluetoothAdapter.isEnabled()) {
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-
         }
     }
 
@@ -43,15 +48,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        switch (requestCode){
+        switch (requestCode) {
             case REQUEST_ENABLE_BT:
                 if (resultCode == Activity.RESULT_OK) {
-                    Toast.makeText(this, "Bluetooth is active", Toast.LENGTH_LONG).show();
+                    // Bluetooth is now enabled, so set up a chat session
+                    Toast.makeText(this, "Bluetooth is active!", Toast.LENGTH_LONG).show();
                 } else {
+                    // User did not enable Bluetooth or an error occurred
                     Log.d(TAG, "BT not enabled");
-                    Toast.makeText(this, "El Bluetooth no s'ha activat correctament", Toast.LENGTH_LONG).show();
+                    Toast.makeText(this, "Bluetooth no s'ha activat correctament!",
+                            Toast.LENGTH_SHORT).show();
                     this.finish();
                 }
+                break;
         }
     }
 
@@ -60,5 +69,20 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
 
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(
+                R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+
+
+        return super.onOptionsItemSelected(item);
     }
 }
