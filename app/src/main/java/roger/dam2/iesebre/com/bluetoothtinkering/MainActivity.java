4ea7163fc34bd4d1d23f1a2b72f2241430f959ac
rogermelich <rogermelich@gmail.com>
2016-02-16 19:06:55 +0100

    Add bluetooth adapter onCreate

--- app/src/main/java/roger/dam2/iesebre/com/bluetoothtinkering/MainActivity.java
+++ app/src/main/java/roger/dam2/iesebre/com/bluetoothtinkering/MainActivity.java
@@ -1,7 +1,8 @@
 package roger.dam2.iesebre.com.bluetoothtinkering;
 
-import android.support.v7.app.AppCompatActivity;
+import android.bluetooth.BluetoothAdapter;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+        // Device does not support Bluetooth
+        }
+
     }
 }
